be1f3a0b0a3c3ff29c1a55069768a523c0e89e68
Hybris95 <hybris_95@hotmail.com>
2016-11-21 18:05:45 +0100

    Initial Release

---
+++ Bot.js
@@ -0,0 +1,55 @@
+var Discord = require('discord.js');
+var PlugAPI = require('plugapi');
+var config = require('config');
+
+/**
+ * Configuration
+ * https://eslachance.gitbooks.io/discord-js-bot-guide/content/getting-started/the-long-version.html
+ */
+var mailPlugDJ = config.get('PlugDJ.Mail');
+var pwdPlugDJ = config.get('PlugDJ.Password');
+var roomPlugDJ = config.get('PlugDJ.Room');
+var textChannelDiscord = config.get('Discord.TextChannel');
+var tokenDiscord = config.get('Discord.Token');
+
+var client;
+var channel;
+var advanceFunction = function(data)
+{
+	if(data.media){
+		var author = data.media.author;
+		var title = data.media.title;
+		if(channel){
+			channel.sendMessage("!play " + author + " " + title)
+		}
+	}
+}
+function outputSuccess(token)
+{
+	console.log(`Logged in. Token: ${token}`);
+	channel = client.channels.get(textChannelDiscord);
+}
+function outputError(error)
+{
+	if(error){
+		console.log(`There was an error : ${error}`);
+		return;
+	}
+}
+
+/**
+ * PlugDJ Bot
+ */
+var botPlug = new PlugAPI({
+	email: mailPlugDJ,
+	password: pwdPlugDJ
+});
+botPlug.connect(roomPlugDJ);
+botPlug.on('advance', advanceFunction);
+
+/**
+ * Discord Bot
+ */
+client = new Discord.Client();
+var promise = client.login(tokenDiscord);
+promise.then(outputSuccess, outputError)
