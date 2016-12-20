4af31cf00a83197e8eee7b3356852a5256c81c94
cbuisson <cbuisson@deltadore.com>
2016-12-20 14:33:04 +0100

    Corrects how joining the waitlist happens

--- Bot.js
+++ Bot.js
@@ -11,6 +11,7 @@ var pwdPlugDJ = config.get('PlugDJ.Password');
 var roomPlugDJ = config.get('PlugDJ.Room');
 var textChannelDiscord = config.get('Discord.TextChannel');
 var tokenDiscord = config.get('Discord.Token');
+var defaultPlaylistId = config.get('PlugDJ.DefaultPlaylistId');
 
 var client;
 var channel;
@@ -24,12 +25,31 @@ var advanceFunction = function(data)
 			channel.sendMessage(djUsername + " just issued " + title + " from PlugDJ room : " + roomPlugDJ);
 			channel.sendMessage("!play " + author + " " + title)
 		}
-	} else {
-		var waitlist = botPlug.getWaitList();
-		if(waitlist.length == 0){
-			botPlug.joinBooth();
-		}
 	}
+	setTimeout(joinBoothIfNone, 5000);
+}
+function joinBoothIfNone(){
+	var waitList = botPlug.getWaitList();
+	var currentDJ = botPlug.getDJ();
+	if(waitList.length == 0 && currentDJ == null){
+		botPlug.getActivePlaylist(joinBoothWithActivePlayList);
+	}
+}
+function joinBoothWithActivePlayList(activePlaylist){
+		if(!activePlaylist){
+			botPlug.activatePlaylist(defaultPlaylistId, joinBoothIfActivated);
+		} else {
+			joinBoothIfActivated(activePlaylist);
+		}
+}
+function joinBoothIfActivated(activePlaylist){
+	if(activePlaylist){
+		var idPlaylist = activePlaylist.id;
+		botPlug.shufflePlaylist(idPlaylist, joinAfterShuffle);
+	}
+}
+function joinAfterShuffle(){
+	botPlug.joinBooth();
 }
 function outputSuccess(token)
 {
